143df400631d81beb66050403db08096e5c0bd48
cassymarie <cassy2205@gmail.com>
2020-10-08 12:10:17 -0600

    Learn.co IDE error, could not push to Repository.  Manually done here

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,14 @@
 # Code your solution here!
+def run_guessing_game
+    random_number = rand(6)+1
+    puts "Please guess a number between 1 and 6"
+    user_guess = gets.chomp
+  
+    if user_guess.to_s == 'exit'
+      print "Goodbye!"
+    elsif user_guess.to_i != random_number
+      print "Sorry! The computer guessed #{random_number}."
+    elsif user_guess.to_i == random_number
+      print "You guessed the correct number!"
+    end
+  end
